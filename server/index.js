c5f8b274d97a37f224c6faa8a80a035ae9d70a54
Asli Tavasli <tavasliasli@gmail.com>
2025-01-26 17:34:34 -0500

    cors fix v2 from use cors

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', routes);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://mymelody-3sak.onrender.com/"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
